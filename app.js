a09229b7030ea8d60b371768a17738f310e8e963
Krystian Paryła <krystian.paryla@o2.pl>
2025-05-01 13:17:58 +0200

    login poage update

--- app.js
+++ app.js
@@ -1,10 +1,12 @@
-window.addEventListener('hashchange', loadPage);
-window.addEventListener('DOMContentLoaded', loadPage);
-
 function loadPage() {
   const view = (location.hash.split('?')[0] || '#start').slice(1);
 
-  if (!window.auth.currentUser && view !== 'start') {
+  if (window.auth.currentUser && (view === 'start' || view === '')) {
+    location.hash = '#rentals';
+    return;
+  }
+
+  if (!window.auth.currentUser && view !== 'start' && view !== 'login') {
     location.hash = '#start';
     return;
   }
@@ -14,12 +16,13 @@ function loadPage() {
     .then(async html => {
       document.getElementById('app').innerHTML = html;
 
-      // dynamiczne importy dla widoków
       const module = await import('./js/fetchData.js');
       if (view === 'vehicles') module.loadVehicles();
       if (view === 'vehicle-details') module.loadVehicleDetails();
     });
 }
 
-// udostępnij globalnie, aby firebase-config.js mógł z tego korzystać
-window.loadPage = loadPage;
+function showLogin() {
+  document.getElementById('loginPanel')?.classList.add('show');
+  navigator.vibrate?.([50]);
+}
